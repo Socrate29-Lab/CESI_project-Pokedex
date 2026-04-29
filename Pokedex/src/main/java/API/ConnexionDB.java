3661b35c7baa2e8f16a5bb251fe936078fd39331
Guillaume <sorgues29@gmail.com>
2026-04-29 09:56:37 +0200

    modifié le MCD (passé tous les ID en int, c'était une erreur), généré le MCD en txt, DB mise en place

--- Pokedex/src/main/java/API/ConnexionDB.java
+++ Pokedex/src/main/java/API/ConnexionDB.java
@@ -5,7 +5,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ConnexionDB {
     private Connection conn = null;
 
-    //Constructeur de la connexion
+    //Constructor de la connexion
     public ConnexionDB(){
         //Pour utiliser le .env
         final Dotenv dotenv = Dotenv.load();
@@ -13,5 +13,10 @@ public class ConnexionDB {
         String url = dotenv.get("DB_URL");
         String user = dotenv.get("DB_USER");
         String password = dotenv.get("DB_PASSWORD");
+        try{
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
